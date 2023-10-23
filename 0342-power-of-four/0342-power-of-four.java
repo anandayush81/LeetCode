4bc73d57771b710aa67d43dd017c395c3b6e9b5f
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2023-10-24 02:37:03 +0530

    Time: 1 ms (76.18%), Space: 40.2 MB (6.4%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -8,19 +8,18 @@ class Solution {
         // if((n&(n-1))!=0) return false;
         // return (n-1)%3==0;
 
+        return (Math.log10(n) / Math.log10(4)) % 1 == 0;
 
-        if (n <= 0) {
-            return false;
-        }
-        
-        int a = (int) (Math.log(n) / Math.log(4));
-        /*
-            4^a = n
-            a = log(n) to base 4
-            a = log(n) to base e / log(4) to base e
-        */
-        
-        return n == Math.pow(4, a);
+        // if (n <= 0) {
+        //     return false;
+        // }
+        // int a = (int) (Math.log(n) / Math.log(4));
+        // /*
+        //     4^a = n
+        //     a = log(n) to base 4
+        //     a = log(n) to base e / log(4) to base e
+        // */
+        // return n == Math.pow(4, a);
 
         // int cnt=0;
         // for(int i=0;i<32;i++){
