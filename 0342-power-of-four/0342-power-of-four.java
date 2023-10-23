961bf9d2bea4e2630b8232488c4b6a47d6c1ea22
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2023-10-23 20:02:11 +0530

    Time: 1 ms (76.31%), Space: 37.8 MB (99.98%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -4,17 +4,22 @@ class Solution {
         // if(n==0) return false;
         // return Math.floor(Math.log(n)/Math.log(4))==Math.ceil(Math.log(n)/Math.log(4));
 
-        int cnt=0;
-        for(int i=0;i<32;i++){
-            if((n&(1<<i))!=0)
-            cnt++;
-        }
-        if(cnt!=1) return false;
-        for(int i=0;i<32;i+=2){
-            if((n&(1<<i))!=0)
-            return true;
-        }
-        return false;
+
+        if((n&(n-1))!=0) return false;
+        return (n-1)%3==0;
+
+
+        // int cnt=0;
+        // for(int i=0;i<32;i++){
+        //     if((n&(1<<i))!=0)
+        //     cnt++;
+        // }
+        // if(cnt!=1) return false;
+        // for(int i=0;i<32;i+=2){
+        //     if((n&(1<<i))!=0)
+        //     return true;
+        // }
+        // return false;
         
     }
 }
