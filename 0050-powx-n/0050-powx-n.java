6bf57f80773b8957df4e066f8cc52ceacab18ee2
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2023-10-25 18:44:54 +0530

    Time: 20 ms (8.37%), Space: 42.9 MB (5.46%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,29 @@
 class Solution {
+    private double solve(double x, int n){
+        System.out.println(" x is "+x +" n is "+n);
+        if(x==0.0) return x;
+        if(n==-1) return 1/x;
+        if(n==0) return 1.0;
+
+        double a=solve(x, n/2);
+        // if(n%2==0) return a*a;
+        //     else return a*a*(1/x);
+
+        if(n<0){
+            if(n%2==0){
+                return (a*a);
+            }
+            else return (a*a*(1/x));
+        }
+        else{
+            if(n%2==0) return a*a;
+            else return a*a*x;
+        }
+        
+    }
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        // return Math.pow(x,n);
+        return solve(x,n);
+
     }
 }
