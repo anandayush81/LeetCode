32d60a69189fb759354750f4175ca70294f06d5e
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2023-10-31 19:33:12 +0530

    Time: 25 ms (6.02%), Space: 50.3 MB (6.16%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -2,9 +2,8 @@ class Solution {
     private boolean solve(String s, int l, int r, int cnt){
         if(l>=r) return true;
         if( (s.charAt(l)!=s.charAt(r) ) && cnt<=0 ) return false;
-
         if(s.charAt(l)==s.charAt(r)) return solve(s,l+1,r-1,cnt);
-        else if( solve(s,l,r-1,0) || solve(s,l+1,r,0) ) return true;
+        else if( solve(s,l,r-1,cnt-1) || solve(s,l+1,r,cnt-1) ) return true;
         return false;
     }
     public boolean validPalindrome(String s) {
