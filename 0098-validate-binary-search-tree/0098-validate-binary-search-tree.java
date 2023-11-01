fdc04d4dee48bc182117409432a082bbfbcfdc7e
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2023-11-02 02:33:31 +0530

    Time: 0 ms (100%), Space: 43.9 MB (30.57%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,33 +13,54 @@
  *     }
  * }
  */
-class Solution {
-    private boolean check(TreeNode root, long mini, long maxi){
-        if(root==null) return true;
-
-        // if(root.val<mini || root.val>maxi) return false;
-
-        // if( (check(root.left,mini,root.val-1) && check(root.right,root.val+1,maxi) )) return true;
-
-        // return false;
 
 
-        // if(root.val>=mini && root.val<=maxi
-        // && check(root.left,mini,root.val-1)
-        // && check(root.right,root.val+1,maxi))
-        // return true;
-        // return false;
+ //one case of 
+ //  -2147483647
+ //   /
+ //  -2147483647
+ //This case is tricky and edge case
+// class Solution {
+//     private boolean check(TreeNode root, long mini, long maxi){
+//         if(root==null) return true;
 
-        if(root.val>mini && root.val<maxi
-        && check(root.left,mini,root.val)
-        && check(root.right,root.val,maxi))
-        return true;
-        return false;
-    }
+//         // if(root.val<mini || root.val>maxi) return false;
+//         // if( (check(root.left,mini,root.val-1) && check(root.right,root.val+1,maxi) )) return true;
+//         // return false;
+
+
+//         // if(root.val>=mini && root.val<=maxi
+//         // && check(root.left,mini,root.val-1)
+//         // && check(root.right,root.val+1,maxi))
+//         // return true;
+//         // return false;
+
+//         if(root.val>mini && root.val<maxi
+//         && check(root.left,mini,root.val)
+//         && check(root.right,root.val,maxi))
+//         return true;
+//         return false;
+//     }
+//     public boolean isValidBST(TreeNode root) {
+//         if(root ==null) return false;
+//         // int mini=Integer.MIN_VALUE, maxi=Integer.MAX_VALUE;
+//         long mini=Long.MIN_VALUE, maxi=Long.MAX_VALUE;
+//         return check(root, mini, maxi);
+//     }
+// }
+
+//another good solution
+public class Solution {
     public boolean isValidBST(TreeNode root) {
-        if(root ==null) return false;
-        // int mini=Integer.MIN_VALUE, maxi=Integer.MAX_VALUE;
-        long mini=Long.MIN_VALUE, maxi=Long.MAX_VALUE;
-        return check(root, mini, maxi);
+        return helper(root, null, null);
+    }
+    boolean helper(TreeNode root, Integer min, Integer max) {
+        if (root == null)
+            return true;
+        
+        if ((min != null && root.val <= min) || (max != null && root.val >= max))
+            return false;
+        
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
     }
 }
