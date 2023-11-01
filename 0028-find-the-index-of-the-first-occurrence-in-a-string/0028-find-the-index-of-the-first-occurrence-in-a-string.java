b78052ad0b923bb02314267e2d218bc16a24fe2e
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2023-11-01 11:14:25 +0530

    Time: 0 ms (100%), Space: 40.2 MB (83.54%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -4,22 +4,48 @@
 //     }
 // }
 
+// class Solution {
+//     public int strStr(String s2, String s1) {
+//         int n1=s1.length(), n2=s2.length();
+//         for(int i=0;i<n2;i++){
+//             if(s2.charAt(i)==s1.charAt(0)){
+//                 int k=0;
+//                 while(i+k<n2 && k < n1){
+//                     if(s2.charAt(i+k)!=s1.charAt(k)){
+//                         break;
+//                     }
+//                     k++;
+//                 }
+//                 if(k==n1) return i;
+//             }
+//         }
+//         return -1;
+        
+//     }
+// }
+
 class Solution {
-    public int strStr(String s2, String s1) {
-        int n1=s1.length(), n2=s2.length();
-        for(int i=0;i<n2;i++){
-            if(s2.charAt(i)==s1.charAt(0)){
-                int k=0;
-                while(i+k<n2 && k < n1){
-                    if(s2.charAt(i+k)!=s1.charAt(k)){
-                        break;
-                    }
-                    k++;
-                }
-                if(k==n1) return i;
+    public int strStr(String haystack, String needle) {
+        int hLen = haystack.length();
+        int nLen = needle.length();
+        int nIndex = 0;
+        for(int i=0; i<hLen; i++){
+            // as long as the characters are equal, increment needleIndex
+            if(haystack.charAt(i)==needle.charAt(nIndex)){
+                nIndex++;
+            }
+            else{
+                // start from the next index of previous start index
+                i=i-nIndex;
+                // needle should start from index 0
+                nIndex=0;
+            }
+            // check if needleIndex reached needle length
+            if(nIndex==nLen){
+                // return the first index
+                return i-nLen+1;
             }
         }
         return -1;
-        
     }
 }
