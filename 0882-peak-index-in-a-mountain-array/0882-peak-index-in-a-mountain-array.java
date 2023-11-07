4a9d10c6b643d4daa59170745c068f1bdf1f755e
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2023-11-07 22:53:31 +0530

    Time: 0 ms (100%), Space: 55 MB (71.62%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -19,9 +19,12 @@ class Solution {
         int l=1,r=n-2;
         while(l<=r){
             int mid=(l+r)/2;
-            if(  a[mid]>a[mid-1] && a[mid]>a[mid+1]) return mid;
-            else if(a[mid] >a[mid-1] && a[mid]<a[mid+1]) l=mid+1;
-            else  r=mid-1;
+            if( a[mid]>a[mid-1] && a[mid]>a[mid+1]) 
+            return mid;
+            else if(a[mid] >a[mid-1] && a[mid]<a[mid+1]) 
+            l=mid+1;
+            else  
+            r=mid-1;
         }
         return -1;        
     }
