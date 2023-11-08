7a8c642dd1fa68b7393b6761a61ca0c2867ae7b2
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2023-11-08 23:59:30 +0530

    Time: 5 ms (44.44%), Space: 40.6 MB (61.98%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -1,56 +1,37 @@
-class Solution {
-    public String reorganizeString(String S) {
-
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : S.toCharArray()) {
-            int count = map.getOrDefault(c, 0) + 1;
-         
-            if (count > (S.length() + 1) / 2) 
-                return "";
-            
-            map.put(c, count);
+public class Solution {
+    public String reorganizeString(String s) {
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
         }
 
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        for (char c : map.keySet()) {
-            pq.add(new int[] {map.get(c), c});
-        }
-        
-        // Build the result.
-        StringBuilder sb = new StringBuilder();
-        while (pq.size() >= 2) {
-            int[] first  = pq.poll();
-            int[] second = pq.poll();
-            
-            sb.append((char) first[1]);
-            sb.append((char) second[1]);
-            
-            first[0]--;
-            second[0]--;
-            
-            if(first[0] > 0)
-                pq.add(first);
-            
-            if(second[0] > 0)
-                pq.add(second);
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> freqMap.get(b) - freqMap.get(a));
+        maxHeap.addAll(freqMap.keySet());
 
+        StringBuilder res = new StringBuilder();
+        while (maxHeap.size() >= 2) {
+            char char1 = maxHeap.poll();
+            char char2 = maxHeap.poll();
+
+            res.append(char1);
+            res.append(char2);
+
+            freqMap.put(char1, freqMap.get(char1) - 1);
+            freqMap.put(char2, freqMap.get(char2) - 1);
+
+            if (freqMap.get(char1) > 0) maxHeap.add(char1);
+            if (freqMap.get(char2) > 0) maxHeap.add(char2);
         }
-        if(!pq.isEmpty()) {
-            int[] first  = pq.poll();
-            
-            sb.append((char) first[1]);
+
+        if (!maxHeap.isEmpty()) {
+            char ch = maxHeap.poll();
+            if (freqMap.get(ch) > 1) return "";
+            res.append(ch);
         }
-        
-        return sb.toString();
+
+        return res.toString();
     }
 }
-
-
-
-
-
-
 
 
 
