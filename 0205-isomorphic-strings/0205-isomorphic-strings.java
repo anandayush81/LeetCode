4d84f336ab70b465829c697e06669b406b176222
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2023-11-14 02:29:50 +0530

    Time: 22 ms (13.08%), Space: 41.4 MB (55.26%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,59 @@
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         // String ar[]=s.split(" ");
+//         // int n=ar.length;
+//         int ns=s.length(), nt=t.length();
+//         if(ns!=nt) return false;
+//         if(ns==1) return true;
+
+//         Map<Character, Character>smap=new HashMap<>();
+//         Map<Character, Character>tmap=new HashMap<>();
+//         // int k=0;
+//         char k='a';
+//         for(int i=0;i<ns;i++){
+//             // if(!map.containsKey(ar[i]))
+//             smap.putIfAbsent(s.charAt(i),k);
+//             tmap.putIfAbsent(t.charAt(i),k);
+//             k++;
+//             // if(smap.size()>26 || tmap.size()>26) return false;
+//         }
+//         StringBuilder sb1=new StringBuilder();
+//         StringBuilder sb2=new StringBuilder();
+//         for(int i=0;i<ns;i++){
+//             sb1.append(smap.get(s.charAt(i)));
+//             sb2.append(tmap.get(t.charAt(i)) );
+//         }
+//         if(sb2.toString().equals(sb1.toString())) return true;
+//         return false;
+//     }
+// }
+
+
+
+
+
+
+
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int ns=s.length(), nt=t.length();
+        if(ns!=nt) return false;
+        Map<Character, Character>maps=new HashMap<>();
+        Map<Character, Character>mapt=new HashMap<>();
+        for(int i=0;i<ns;i++){
+            
+            char cs=s.charAt(i), ct=t.charAt(i);
+            
+            if( (maps.containsKey(cs) && maps.get(cs)!=ct) || (mapt.containsKey(ct) && mapt.get(ct)!=cs) ) 
+            return false;
+
+            // maps.putIfAbsent(cs,ct);
+            // mapt.putIfAbsent(ct,cs);
+
+            maps.put(cs,ct);
+            mapt.put(ct,cs);
+        }
+        return true;
+    }
+}
