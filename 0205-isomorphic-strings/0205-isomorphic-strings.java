834279b1286970762312cfe104a84e7fcd63a4fa
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2023-11-18 19:49:57 +0530

    Time: 7 ms (86.35%), Space: 41.3 MB (69.00%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -29,31 +29,46 @@
 // }
 
 
-
-
-
-
-
-
+//comment
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int ns=s.length(), nt=t.length();
-        if(ns!=nt) return false;
-        Map<Character, Character>maps=new HashMap<>();
-        Map<Character, Character>mapt=new HashMap<>();
-        for(int i=0;i<ns;i++){
-            
-            char cs=s.charAt(i), ct=t.charAt(i);
-            
-            if( (maps.containsKey(cs) && maps.get(cs)!=ct) || (mapt.containsKey(ct) && mapt.get(ct)!=cs) ) 
+        int map1[]=new int[200];
+        int map2[]=new int[200];
+        if(s.length()!=t.length())
             return false;
-
-            // maps.putIfAbsent(cs,ct);
-            // mapt.putIfAbsent(ct,cs);
-
-            maps.put(cs,ct);
-            mapt.put(ct,cs);
+        for(int i=0;i<s.length();i++)
+        {
+            if(map1[s.charAt(i)]!=map2[t.charAt(i)])
+                return false;
+            map1[s.charAt(i)]=i+1;
+            map2[t.charAt(i)]=i+1;
         }
         return true;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         int ns=s.length(), nt=t.length();
+//         if(ns!=nt) return false;
+//         Map<Character, Character>maps=new HashMap<>();
+//         Map<Character, Character>mapt=new HashMap<>();
+//         for(int i=0;i<ns;i++){
+            
+//             char cs=s.charAt(i), ct=t.charAt(i);
+            
+//             if( (maps.containsKey(cs) && maps.get(cs)!=ct) || (mapt.containsKey(ct) && mapt.get(ct)!=cs) ) 
+//             return false;
+
+//             // maps.putIfAbsent(cs,ct);
+//             // mapt.putIfAbsent(ct,cs);
+
+//             maps.put(cs,ct);
+//             mapt.put(ct,cs);
+//         }
+//         return true;
+//     }
+// }
