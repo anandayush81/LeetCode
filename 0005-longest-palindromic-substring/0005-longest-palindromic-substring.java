184d4a558fcd2a3e4969e50af0ec4bb555613348
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2024-01-22 17:09:41 +0530

    Time: 19 ms (69.05%), Space: 45 MB (34.60%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,3 +1,108 @@
+public class Solution {
+    private String expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        String maxStr = s.substring(0, 1);
+        for (int i = 0; i < s.length() - 1; i++) {
+            String odd = expandFromCenter(s, i, i);
+            String even = expandFromCenter(s, i, i + 1);
+            if (odd.length() > maxStr.length()) {
+                maxStr = odd;
+            }
+            if (even.length() > maxStr.length()) {
+                maxStr = even;
+            }
+        }
+        return maxStr;
+    }   
+}
+
+
+//My recursion+memoization solution
+/*
+public class Solution {
+    private Boolean dp[][];
+    private Boolean solve(String s, int l, int r) {
+        if (l>=r) {
+            return true;
+        }
+        if (dp[l][r] != null) {
+            return dp[l][r];
+        }
+        if(s.charAt(l)==s.charAt(r)){
+            return dp[l][r]=solve(s,l+1,r-1);
+        }
+        return dp[l][r]=false;
+    }
+
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int maxlen = Integer.MIN_VALUE;
+        int startingIndex = 0;
+        dp=new Boolean[1001][1001];
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solve(s, i, j) && j - i + 1 > maxlen) {
+                    startingIndex = i;
+                    maxlen = j - i + 1;
+                }
+            }
+        }
+        return s.substring(startingIndex, startingIndex + maxlen);
+    }
+}
+
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+class Solution {
+    int l,r;
+    int max_len = 1;int st = 0, end = 0;
+    private void findPalin(String s, int l, int r, int n){
+        while(l >= 0 && r < n){
+                if(s.charAt(l) == s.charAt(r)){
+                    l--; r++;
+                }else
+                    break;
+            }
+        int len = r-l-1;
+        if(len > max_len){
+            max_len = len;
+            st = l+1;
+            end = r-1;
+        }
+    }
+    public String longestPalindrome(String s) {
+        if(s.length() <= 1) return s;
+        int n = s.length();
+        // Odd length
+        for(int i = 0; i<n; ++i){
+            l = i; r = i;
+            findPalin( s,l,r,n);
+        }
+        // Even length
+        for(int i = 0; i < n; ++i){
+            l = i; r = i+1;
+            findPalin( s,l,r,n);
+        }
+        return s.substring(st, end + 1);
+    }
+}
+
+*/
+
+
+/*
 class Solution {
     public String longestPalindrome(String s) {
         int n=s.length();
@@ -36,26 +141,6 @@ class Solution {
             k++;
         }
         return s.substring(stind, endind+1);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         // int n=s.length();
         // String s2="";
@@ -104,3 +189,5 @@ class Solution {
         // return str;
     }
 }
+
+*/
