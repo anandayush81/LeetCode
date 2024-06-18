0b45007c047603c27b3bdbbb0d07ea2157962d9a
Ayush Anand <39666801+anandayush81@users.noreply.github.com>
2024-06-18 21:05:24 +0530

    Time: 74 ms (21.99%), Space: 46.6 MB (24.08%) - LeetHub

---
+++ 1905-design-authentication-manager/1905-design-authentication-manager.java
@@ -0,0 +1,79 @@
+// class AuthenticationManager {
+
+//     public AuthenticationManager(int timeToLive) {
+        
+//     }
+    
+//     public void generate(String tokenId, int currentTime) {
+        
+//     }
+    
+//     public void renew(String tokenId, int currentTime) {
+        
+//     }
+    
+//     public int countUnexpiredTokens(int currentTime) {
+        
+//     }
+// }
+
+
+class AuthenticationManager {
+
+    int time=0;
+
+    HashMap<String,Integer>mp=new HashMap<>();
+
+    public AuthenticationManager(int timeToLive) {
+        
+    time=timeToLive;
+
+    }
+    
+    public void generate(String tokenId, int currentTime) {
+        
+    mp.put(tokenId,currentTime+time);
+
+    }
+    
+    public void renew(String tokenId, int currentTime) {
+        
+    if(!mp.containsKey(tokenId)||mp.get(tokenId)<=currentTime)
+    {
+    return;
+    }
+
+    mp.put(tokenId,currentTime+time);
+
+    }
+    
+    public int countUnexpiredTokens(int currentTime) {
+    
+    List<String>lr=new ArrayList<>(mp.keySet());
+    
+    for(int i=0;i<lr.size();i++){
+    
+    if(mp.get(lr.get(i))<=currentTime)mp.remove(lr.get(i));
+
+    }
+
+    return mp.size();
+    
+    }
+}
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * AuthenticationManager obj = new AuthenticationManager(timeToLive);
+ * obj.generate(tokenId,currentTime);
+ * obj.renew(tokenId,currentTime);
+ * int param_3 = obj.countUnexpiredTokens(currentTime);
+ */
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * AuthenticationManager obj = new AuthenticationManager(timeToLive);
+ * obj.generate(tokenId,currentTime);
+ * obj.renew(tokenId,currentTime);
+ * int param_3 = obj.countUnexpiredTokens(currentTime);
+ */
