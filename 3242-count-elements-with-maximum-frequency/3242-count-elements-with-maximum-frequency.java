32386a27c8280bc0b30c36fc5611b60dd310907a
anandayush81 <39666801+anandayush81@users.noreply.github.com>
2025-10-21 15:55:31 +0530

    Time: 3 ms (23.26%), Space: 41.6 MB (99.87%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
@@ -1,3 +1,23 @@
+// class Solution {
+//     public int maxFrequencyElements(int[] nums) {
+//         int n=nums.length;
+//         int ans=0;
+//         int mxcnt=0;
+//         Map<Integer, Integer>mp=new HashMap<>();
+//         for(int x:nums){
+//             mp.put(x, mp.getOrDefault(x,0)+1);
+//             if (mxcnt<mp.get(x)) mxcnt=mp.get(x);
+
+//         }
+//         for(int key:mp.keySet()){
+//             if(mp.get(key)==mxcnt) ans+=mp.get(key);
+//         }
+//         return ans;
+        
+//     }
+// }
+
+
 class Solution {
     public int maxFrequencyElements(int[] nums) {
         int n=nums.length;
@@ -6,12 +26,13 @@ class Solution {
         Map<Integer, Integer>mp=new HashMap<>();
         for(int x:nums){
             mp.put(x, mp.getOrDefault(x,0)+1);
-            if (mxcnt<mp.get(x)) mxcnt=mp.get(x);
-
+            if (mxcnt<mp.get(x)) {
+                mxcnt=mp.get(x);
+                ans=mxcnt;
+            }
+            else if (mp.get(x)==mxcnt) ans+=mxcnt;
         }
-        for(int key:mp.keySet()){
-            if(mp.get(key)==mxcnt) ans+=mp.get(key);
-        }
+        
         return ans;
         
     }
