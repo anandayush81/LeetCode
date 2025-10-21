61ec94e74a7c9ea21a77b6fba03cb9b2055d277e
anandayush81 <39666801+anandayush81@users.noreply.github.com>
2025-10-21 15:56:06 +0530

    Time: 2 ms (61.81%), Space: 42.6 MB (20.77%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
@@ -7,7 +7,6 @@
 //         for(int x:nums){
 //             mp.put(x, mp.getOrDefault(x,0)+1);
 //             if (mxcnt<mp.get(x)) mxcnt=mp.get(x);
-
 //         }
 //         for(int key:mp.keySet()){
 //             if(mp.get(key)==mxcnt) ans+=mp.get(key);
@@ -17,6 +16,8 @@
 //     }
 // }
 
+
+//single pass 
 
 class Solution {
     public int maxFrequencyElements(int[] nums) {
