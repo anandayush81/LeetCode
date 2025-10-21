a3243bc35b25a226e660325d2c7fe394a061b187
anandayush81 <39666801+anandayush81@users.noreply.github.com>
2025-10-21 22:01:40 +0530

    Time: 387 ms (5%), Space: 45.9 MB (32.17%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -14,20 +14,27 @@
 //     }
 // }
 
+//memoization
+
+//here dp[i] indicates if it's possible to reach the last index from the index i
+//And since we're returning the final result as dp[0]- the final result indicates if it is possible to reach the last index from 0th index
+
+
 class Solution {
-    public boolean solve(boolean dp[], int nums[], int idx){
-        if(idx==(nums.length-1)) return dp[idx]= true;
-        if(nums[idx]==0) dp[idx]= false;
-        for(int i=1;i<=Math.min(nums.length, nums[idx]);i++){
-            if(solve(dp, nums, idx+i)==true)
-            return dp[idx]=true;
+    Boolean dp[];
+    private boolean solve(int nums[], int idx, int n){
+        if(idx>=n) return false;
+        if(idx==n-1) return true;
+        if(nums[idx]==0) return false;
+        if(dp[idx]!=null) return dp[idx];
+        for(int i=1;i<=nums[idx];i++){
+            if(solve(nums,idx+i,n)) return dp[idx]=true;
         }
         return dp[idx]=false;
     }
     public boolean canJump(int[] nums) {
-        boolean dp[]=new boolean[nums.length];
-        Arrays.fill(dp, false);
-        return solve(dp, nums,0);
-        
+        dp=new Boolean[nums.length];
+        return solve(nums, 0, nums.length);
+
     }
 }
