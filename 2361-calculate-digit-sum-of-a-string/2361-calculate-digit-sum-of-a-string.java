40a4cb170132b4ab3cfa6d05baf56b7f8df7a2b8
anandayush81 <39666801+anandayush81@users.noreply.github.com>
2025-10-24 23:23:50 +0530

    Time: 1 ms (95.18%), Space: 41.9 MB (43.89%) - LeetHub

--- 2361-calculate-digit-sum-of-a-string/2361-calculate-digit-sum-of-a-string.java
+++ 2361-calculate-digit-sum-of-a-string/2361-calculate-digit-sum-of-a-string.java
@@ -1,21 +1,63 @@
+// class Solution {
+//     public String digitSum(String s, int k) {
+//         int n=s.length(); String str=s;
+//         while(str.length()>k){
+//             char ar[]=str.toCharArray();
+//             String temp="";
+//             int i=0;
+//             while(i<ar.length){
+//                 int sum=0;
+//                 for(int j=i;j<(i+k) && j<ar.length;j++){
+//                     sum+=(ar[j]-'0');
+//                 }
+//                 // System.out.println("sum="+sum);
+//                 temp+=""+sum;
+//                 i+=k;
+//             }
+//             // System.out.println("str="+str);
+//             str=temp;
+//         }
+//         return str;
+        
+//     }
+// }
+
 class Solution {
+    private String modify(String s2){
+        int l=s2.length();
+        int sum=0;
+        for (int i=0;i<l;i++){
+            sum=sum + (s2.charAt(i)-'0');
+        }
+        return ""+sum;
+        
+    } 
     public String digitSum(String s, int k) {
-        int n=s.length(); String str=s;
+        int n=s.length();
+        String str=s;
         while(str.length()>k){
-            char ar[]=str.toCharArray();
-            String temp="";
+            int len=str.length();
+            String currtemp="";
             int i=0;
-            while(i<ar.length){
-                int sum=0;
-                for(int j=i;j<(i+k) && j<ar.length;j++){
-                    sum+=(ar[j]-'0');
+            while(i<len){
+                String s1="";
+                if(i+k<=len){
+                    s1=str.substring(i,i+k);
+                    i+=k;
+
                 }
-                // System.out.println("sum="+sum);
-                temp+=""+sum;
-                i+=k;
+                else{
+                    s1=str.substring(i,len);
+                    i+=k;
+
+                }
+                
+                currtemp+=modify(s1);
+                // System.out.println("currtemp="+currtemp);
+
             }
+            str=currtemp;
             // System.out.println("str="+str);
-            str=temp;
         }
         return str;
         
