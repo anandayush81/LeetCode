56d7e56492b2b347dc6a62159f0af4800e885d1b
anandayush81 <39666801+anandayush81@users.noreply.github.com>
2025-10-25 02:15:10 +0530

    Time: 62 ms (14.3%), Space: 57.5 MB (99.48%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.java
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.java
@@ -1,44 +1,26 @@
-//instead of trying to find the minimum number of operations, why don't we focus on finding the longest subarray in the middle. One main thing to note is that our subarray should sum to sum - x (where sum is the sum of all elements in our array).
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int n=nums.length;
-        int sum=0;
-        for(int e:nums){
-            sum+=e;
+        long sum=0; int n=nums.length;
+        for (int i=0;i<n;i++){
+            sum+=nums[i];
         }
         if(x>sum) return -1;
-        int k=sum-x;
-        //find pf sum
-        int pf[]=new int[n+1];
-        for(int i=1;i<=n;i++){
-            pf[i]=pf[i-1]+nums[i-1];
-        }
-        // System.out.println(Arrays.toString(pf));
-        int i=0,j=0;
-        int maxlen=Integer.MIN_VALUE;
-        int len=0;
-        int flag=0;
-        while(j<=n && i<=n){
-            if(pf[j]-pf[i]==k){
-                len=j-i;
-                maxlen=Math.max(maxlen, len);
-                i++;
-                j++;
-                
-            }
-            else if(pf[j]-pf[i]<k)
-                j++;
-            else{
-                i++;
-                if(i==j)
-                j++;
-            }
-
-        }
-        if(maxlen==Integer.MIN_VALUE) return -1;
-        return n-maxlen;
+        if(x==sum) return n;
+        long target = sum-x;
+        int maxi=Integer.MIN_VALUE;
+        Map<Long, Integer>mp=new HashMap<>();
+        long s=0;
+        mp.put(0L,-1);
         
-
-
+        for(int i=0;i<n;i++){
+            s=s+nums[i];
+            if(mp.containsKey(s-target)){
+                maxi=Math.max(maxi, (i-mp.get(s-target)));
+            }
+            if(!mp.containsKey(s)) mp.put(s,i);
+        }
+        if(maxi==Integer.MIN_VALUE) return -1;
+        return n-maxi;
+        
     }
 }
