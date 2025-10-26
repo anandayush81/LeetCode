82d80fd96325797c35235e2bc65f353ce8ce64e5
anandayush81 <39666801+anandayush81@users.noreply.github.com>
2025-10-26 22:02:16 +0530

    Time: 100 ms (41.42%), Space: 46.3 MB (5.78%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,3 +1,67 @@
+//BF
+// class Solution {
+//     private int solve(int nums[], int n, int idx, int sum, int target){
+//         if(idx == n){
+//             if(sum == target) return 1;
+//             return 0;
+//         }
+//         int add=solve(nums,n, idx+1, sum+nums[idx], target);
+//         int sub=solve(nums,n, idx+1, sum-nums[idx], target);
+//         return add+sub;
+        
+//     }
+//     public int findTargetSumWays(int[] nums, int target) {
+//         Map<String, Integer> mp=new HashMap<>();
+//         return solve(nums, nums.length, 0, 0, target);
+       
+        
+//     }
+// }
+
+
+//Memoizatiom
+class Solution {
+    private int solve(int nums[], int n, int idx, int sum, int target, Map<String, Integer> mp){
+        if(idx == n){
+            if(sum == target) return 1;
+            return 0;
+        }
+        String key=""+(""+idx)+"_"+(sum+"");
+        if(mp.containsKey(key)) return mp.get(key);
+
+        int add=solve(nums,n, idx+1, sum+nums[idx], target, mp);
+        int sub=solve(nums,n, idx+1, sum-nums[idx], target, mp);
+
+        // String key=""+(""+idx)+"_"+(sum+"");
+        mp.put(key, (sub+add));
+        return add+sub;
+        
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        Map<String, Integer> mp=new HashMap<>();
+        return solve(nums, nums.length, 0, 0, target, mp);
+       
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // class Solution {
 //     int cnt=0;
 //     private void solve(int nums[], int target, List<Integer>temp, int idx, int n){
@@ -28,21 +92,4 @@
 //     }
 // }
 
-class Solution {
-    private int solve(int nums[], int n, int idx, int sum, int target){
-        if(idx == n){
-            if(sum == target) return 1;
-            return 0;
-        }
-        int add=solve(nums,n, idx+1, sum+nums[idx], target);
-        int sub=solve(nums,n, idx+1, sum-nums[idx], target);
-        return add+sub;
-        
-    }
-    public int findTargetSumWays(int[] nums, int target) {
-        Map<String, Integer> mp=new HashMap<>();
-        return solve(nums, nums.length, 0, 0, target);
-       
-        
-    }
-}
+
