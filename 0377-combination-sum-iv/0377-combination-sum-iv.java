c65fcdb24b1d9c8567877f289806376a6e1c4590
anandayush81 <39666801+anandayush81@users.noreply.github.com>
2025-11-02 20:07:00 +0530

    Time: 1 ms (67.78%), Space: 42.9 MB (5.09%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -4,11 +4,17 @@ class Solution {
             if(sum==0) return 1;
             return 0;
         }
+        if(idx>=nums.length || sum<0) return 0;
         if(dp[idx][sum]!=-1) return dp[idx][sum];
-        int ans=0;
-        for(int i=idx;i<nums.length;i++){
-            ans+=solve(nums, dp, target, 0, sum-nums[i]);
-        }
+
+        int take = solve(nums, dp, target, 0, sum-nums[idx]);
+        int donttake=solve(nums, dp, target, idx+1, sum);
+        int ans=take+donttake;
+        
+        // int ans=0;
+        // for(int i=idx;i<nums.length;i++){
+        //     ans+=solve(nums, dp, target, 0, sum-nums[i]);
+        // }
         return dp[idx][sum]=ans;
     }
     public int combinationSum4(int[] nums, int target) {
@@ -17,7 +23,15 @@ class Solution {
         for(int x[]:dp){
             Arrays.fill(x,-1);
         }
-        return solve(nums, dp, target, 0, target);
+        int ans=  solve(nums, dp, target, 0, target);
+        // System.out.println(Arrays.toString(dp));
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=target;j++){
+        //         System.out.print(dp[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+        return ans;
         
     }
 }
